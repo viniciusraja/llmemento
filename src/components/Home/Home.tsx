093210fcd52a8d878d2f2b82b451104e4c79e1d2
viniciusraja <viniciusrfl@hotmail.com>
2023-08-03 19:49:57 -0300

    feat: show template editor only after upload

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,19 @@
 import { Box, VStack } from "@chakra-ui/react";
 import TemplateEditor from "../TemplateEditor";
+import useUploadedTemplateEditorStore from "../TemplateUploader/store/useUploadedTemplateEditorStore";
+import TemplateUploader from "../TemplateUploader";
 
 const Home = () => {
+  const uploadedTemplateEditor = useUploadedTemplateEditorStore(
+    (state) => state.uploadedTemplateEditor
+  );
+
+  const isUploadedTemplateEmpty =
+    Object.keys(uploadedTemplateEditor).length === 0;
+
   return (
-    <VStack w="100%">
-      <TemplateEditor />
+    <VStack w="100%" bg="red">
+      {isUploadedTemplateEmpty ? <TemplateUploader /> : <TemplateEditor />}
     </VStack>
   );
 };
