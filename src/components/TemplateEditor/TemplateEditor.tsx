8ad5c1b0e578c8a49468f371c6fd266889106938
viniciusraja <viniciusrfl@hotmail.com>
2023-08-03 19:33:55 -0300

    feat: adjust styles to fit screen

--- src/components/TemplateEditor/TemplateEditor.tsx
+++ src/components/TemplateEditor/TemplateEditor.tsx
@@ -3,33 +3,49 @@ import ImageTemplateEditorElement from "./ImageTemplateEditorElement";
 import { TemplateElement } from "./templateTypes";
 import BackgroundTemplateEditorElement from "./BackgroundTemplateEditorElement";
 import TextTemplateEditorElement from "./TextTemplateEditorElement";
+import { Box, HStack } from "@chakra-ui/react";
+import TemplateEditorForm from "./TemplateEditorForm";
+import useUploadedTemplateEditorStore from "../TemplateUploader/store/useUploadedTemplateEditorStore";
 
 const TemplateEditor = () => {
+  const uploadedTemplateEditor = useUploadedTemplateEditorStore(
+    (state) => state.uploadedTemplateEditor
+  );
+
   const elementsToRender = Object.values(
-    file.elements
+    uploadedTemplateEditor?.elements || {}
   ) as any as TemplateElement[];
 
   return (
-    <BackgroundTemplateEditorElement backgroundConfig={file.background as any}>
-      {elementsToRender?.map((elementToRender) => {
-        if (elementToRender.type === "text")
-          return (
-            <TextTemplateEditorElement
-              key={elementToRender?.id}
-              {...elementToRender}
-            />
-          );
+    <HStack w="100%" spacing="0" h="100vh" overflow="hidden">
+      <Box w="50%">
+        <TemplateEditorForm />
+      </Box>
+      <Box w="50%">
+        <BackgroundTemplateEditorElement
+          backgroundConfig={uploadedTemplateEditor?.background as any}
+        >
+          {elementsToRender?.map((elementToRender) => {
+            if (elementToRender.type === "text")
+              return (
+                <TextTemplateEditorElement
+                  key={elementToRender?.id}
+                  {...elementToRender}
+                />
+              );
 
-        if (elementToRender.type === "image")
-          return (
-            <ImageTemplateEditorElement
-              key={elementToRender?.id}
-              {...elementToRender}
-            />
-          );
-        return null;
-      })}
-    </BackgroundTemplateEditorElement>
+            if (elementToRender.type === "image")
+              return (
+                <ImageTemplateEditorElement
+                  key={elementToRender?.id}
+                  {...elementToRender}
+                />
+              );
+            return null;
+          })}
+        </BackgroundTemplateEditorElement>
+      </Box>
+    </HStack>
   );
 };
 
--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -1,30 +1,10 @@
-import { StyleOptions } from './colors';
+import { StyleOptions } from "./colors";
 
 const styles = {
   global: ({ colorMode }: StyleOptions) => ({
     body: {
-      bg: 'white',
-      color: colorMode === 'light' ? 'gray.900' : 'gray.300',
-    },
-    li: {
-      display: 'list-item',
-      textAlign: '-webkit-match-parent',
-    },
-    ul: {
-      display: 'block',
-      marginBlockStart: '0.5em',
-      marginBlockEnd: '0.5em',
-      paddingInlineStart: '40px',
-    },
-    ol: {
-      display: 'block',
-      marginBlockStart: '0.5em',
-      marginBlockEnd: '0.5em',
-      paddingInlineStart: '40px',
-    },
-    em: { fontStyle: 'italic' },
-    p: {
-      fontFamily: 'Fraktion Sans, sans-serif',
+      height: "100vh",
+      overflowX: "hidden",
     },
   }),
 };
