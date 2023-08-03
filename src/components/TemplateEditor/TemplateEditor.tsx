7d6def3e9d3679ee3fd3730ed2cacb316fd3ca71
viniciusraja <viniciusrfl@hotmail.com>
2023-08-03 00:01:58 -0300

    feat: add text template editor

--- src/components/TemplateEditor/TemplateEditor.tsx
+++ src/components/TemplateEditor/TemplateEditor.tsx
@@ -1,8 +1,8 @@
 import file from "~/components/SampleTemplate.json";
 import ImageTemplateEditorElement from "./ImageTemplateEditorElement";
-import { VStack } from "@chakra-ui/react";
 import { TemplateElement } from "./templateTypes";
 import BackgroundTemplateEditorElement from "./BackgroundTemplateEditorElement";
+import TextTemplateEditorElement from "./TextTemplateEditorElement";
 
 const TemplateEditor = () => {
   const elementsToRender = Object.values(
@@ -10,22 +10,26 @@ const TemplateEditor = () => {
   ) as any as TemplateElement[];
 
   return (
-    <VStack w="100%" h="100%">
-      <BackgroundTemplateEditorElement
-        backgroundConfig={file.background as any}
-      >
-        {elementsToRender?.map((elementToRender) => {
-          if (elementToRender.type === "image")
-            return (
-              <ImageTemplateEditorElement
-                key={elementToRender?.id}
-                {...elementToRender}
-              />
-            );
-          return null;
-        })}
-      </BackgroundTemplateEditorElement>
-    </VStack>
+    <BackgroundTemplateEditorElement backgroundConfig={file.background as any}>
+      {elementsToRender?.map((elementToRender) => {
+        if (elementToRender.type === "text")
+          return (
+            <TextTemplateEditorElement
+              key={elementToRender?.id}
+              {...elementToRender}
+            />
+          );
+
+        if (elementToRender.type === "image")
+          return (
+            <ImageTemplateEditorElement
+              key={elementToRender?.id}
+              {...elementToRender}
+            />
+          );
+        return null;
+      })}
+    </BackgroundTemplateEditorElement>
   );
 };
 
