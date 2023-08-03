f4abb0b45177fab2c1a472958d76fdb1d72be40b
viniciusraja <viniciusrfl@hotmail.com>
2023-08-03 19:32:19 -0300

    feat: add CacheProvider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
-
+import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
 import Home from "~/components/Home";
 import theme from "~/theme";
 
 export default function HomePage() {
   return (
-    <ChakraProvider theme={theme}>
-      <Home />
-    </ChakraProvider>
+    <CacheProvider>
+      <ChakraProvider theme={theme}>
+        <Home />
+      </ChakraProvider>
+    </CacheProvider>
   );
 }
