a0a6769a16c2b23c35006650858484b9757c0365
viniciusraja <viniciusrfl@hotmail.com>
2023-08-03 21:25:16 -0300

    feat: purify html before extracting string

--- src/components/TemplateEditor/TemplateEditorForm/utils/getSanitizedString.ts
+++ src/components/TemplateEditor/TemplateEditorForm/utils/getSanitizedString.ts
@@ -1,7 +1,9 @@
+import DOMPurify from "dompurify";
+
 const getSanitizedString = (inputHTML: string) => {
   const tempElement = document.createElement("div");
 
-  tempElement.innerHTML = inputHTML;
+  tempElement.innerHTML = DOMPurify.sanitize(inputHTML);
 
   const sanitizedText = tempElement.textContent || tempElement.innerText; // Use textContent for modern browsers and innerText for older ones
 
