cf2b48f6c3e5dbcd62cf3e2e4747bf8b70d4982b
viniciusraja <viniciusrfl@hotmail.com>
2023-08-14 19:57:09 -0300

    feat: hydrate next with local storage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,17 @@ import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import HydrateStore from "~/components/HydrateStore";
 import theme from "~/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <DndProvider backend={HTML5Backend}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </DndProvider>
+    <HydrateStore>
+      <DndProvider backend={HTML5Backend}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </DndProvider>
+    </HydrateStore>
   );
 }
