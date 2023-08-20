c29a128fcbfd322704d0fbb2e452f81563b14203
viniciusraja <viniciusrfl@hotmail.com>
2023-08-20 15:31:28 -0300

    feat: create template api

--- src/app/api/template/_utils/createTemplate.ts
+++ src/app/api/template/_utils/createTemplate.ts
@@ -1,17 +1,39 @@
 import { PrismaClient } from "@prisma/client";
+import { TemplateData } from "~/components/TemplateEditor/templateTypes";
 
 const prisma = new PrismaClient();
 
 type CreateTemplate = {
-  background: {};
-  elements: {};
+  background: TemplateData["background"];
+  elements: TemplateData["elements"];
 };
 
 const createTemplate = async ({ background, elements }: CreateTemplate) => {
   const createdTemplate = await prisma.template.create({
     data: {
-      background,
-      elements,
+      BackgroundTemplateElement: {
+        create: {
+          Color: {
+            create: {
+              a: background.a,
+              b: background.b,
+              g: background.g,
+              r: background.r,
+            },
+          },
+          PageConfig: {
+            create: {
+              height: background.pageConfig.height,
+              width: background.pageConfig.width,
+              unit: background.pageConfig.unit,
+              dpi: background.pageConfig.dpi,
+            },
+          },
+        },
+      },
+      elements: {
+        createMany: { data: elements },
+      },
     },
   });
 
